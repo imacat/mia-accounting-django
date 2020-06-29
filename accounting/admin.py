cde10aed91f01d5be01cff3fe7eaa6a61c38fb7a
依瑪貓 <imacat@mail.imacat.idv.tw>
2020-06-30 05:41:41 +0800

    Added the accounting models to the administration.

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Subject, Transaction, Record
+
+admin.site.register(Subject)
+admin.site.register(Transaction)
+admin.site.register(Record)
