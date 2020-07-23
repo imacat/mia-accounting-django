10cbe2a70fd4961ad2f484015cb075b9ee7825bb
依瑪貓 <imacat@mail.imacat.idv.tw>
2020-07-24 06:42:33 +0800

    Revised the documentation of the utilities of the Mia core application.

--- mia_core/utils.py
+++ mia_core/utils.py
@@ -15,7 +15,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-"""The views of the Mia core application.
+"""The utilities of the Mia core application.
 
 """
 import random
