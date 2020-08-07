d6e68717fdce8c251d899da1cc3bc8803cfc3971
依瑪貓 <imacat@mail.imacat.idv.tw>
2020-08-07 21:35:03 +0800

    Moved the JavaScript on the account list template to a separated JavaScript file in the accounting application.

---
+++ accounting/static/accounting/js/account-list.js
@@ -0,0 +1,29 @@
+/* The Mia Website
+ * account-list.js: The JavaScript for the account list
+ */
+
+/*  Copyright (c) 2019-2020 imacat.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/* Author: imacat@mail.imacat.idv.tw (imacat)
+ * First written: 2020/8/7
+ */
+
+// Initializes the page JavaScript.
+$(function () {
+    $('#accounts').DataTable({
+        "ordering": false,
+    });
+});
