72886c18f2f8b4a4fc03b2c062762ffad678ada6
依瑪貓 <imacat@mail.imacat.idv.tw>
2020-08-13 07:26:08 +0800

    Removed unused admin packages.

--- accounting/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Account, Transaction, Record
-
-admin.site.register(Account)
-admin.site.register(Transaction)
-admin.site.register(Record)
